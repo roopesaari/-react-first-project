cecad22e82bfc6d48027c8024c6000df5bc058ea
Saari <e2202896@edu.vamk.fi>
2024-09-16 13:55:56 +0300

    tehtävät 3 ja 4

--- src/components/Expenseltem.js
+++ src/components/Expenseltem.js
@@ -1,11 +1,13 @@
+import ExpenseDate from './ExpenseDate';
 import './Expenseltem.css';
-function Expenseltem() {
+function Expenseltem(props) {
+
 return (
     <div className="expense-item">
-        <div>Jan 1th 2023</div>
+       <ExpenseDate date={props.date} />
         <div className="expense-item__desripction">
-            <h2>Car Insurance</h2>
-            <div className="expense-item__price">$294.67</div>
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">${props.amount}</div>
         </div>
     </div>
 );
